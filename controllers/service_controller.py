b8931762be6682bc05ccf3e843b43619d47d43f2
adelghaedi <adelghaedii12@gmail.com>
2025-05-31 12:23:34 +0330

    add service detail template

--- controllers/service_controller.py
+++ controllers/service_controller.py
@@ -1,7 +1,7 @@
 from odoo.http import Controller,route,request
 
 
-class ServiceController(Controller):
+class ServiceListController(Controller):
     @route("/service" , auth="user",type="http",website=True)
     
 
@@ -13,6 +13,24 @@ class ServiceController(Controller):
                 'services':services
             }
         )
+
+
+class ServiceDetailController(Controller):
+    @route('/service/<int:service_id>',type="http",auth="user",website=True)
+
+
+
+    def render_service(self,service_id):
+        service=request.env["appointment.service"].sudo().browse(service_id)
+
+        if not service.exists():
+            return request.not_found()
+        else:
+            return request.render('appointment_module.appointment_service_detail_template',{
+                "service":service,
+            }
+            )
+
 
     
 
