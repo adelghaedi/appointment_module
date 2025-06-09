7feaf89c1908989e268c820d5626a121e1e83fd0
adelghaedi <adelghaedii12@gmail.com>
2025-06-09 09:38:07 +0330

    test unique name for service

--- tests/test_service.py
+++ tests/test_service.py
@@ -24,4 +24,18 @@ class TestService(TransactionCase):
         self.assertFalse(service.image)
         self.assertFalse(service.workfield_id)
         self.assertFalse(service.employee_ids)
+    
+    def test_unique_name(self):
+        self.env["appointment.service"].create(
+            {
+                "name":"Unique Service",
+                "price":5000.0,
+            })
+        with self.assertRaises(Exception):
+            self.env["appointment.service"].create(
+                {
+                    "name":"Unique Service",
+                    "price":3000.0,
+                })
+
         
